81d2109c6f01588aabf07816e5e0f4f933941cb7
FrederickGarcia <frederickgarcia654456@gmail.com>
2013-10-28 12:59:04 -0400

    No

--- bearbot/sub_modules/bot_commands.py
+++ bearbot/sub_modules/bot_commands.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ''' Takes PRIVMSG and calls modules
 Created on 29/10/2013
 
@@ -14,21 +13,4 @@ def privmsg_(handler, msg):
             cmd = Command(handler.bot, msg)
             command_dic[cmd.root](cmd)  # Runs command
         except Exception as e:
-=======
-''' Takes PRIVMSG and calls modules
-Created on 29/10/2013
-
-@author: Evan
-'''
-from bearbot.core.command import *
-from bearbot.sub_modules.ctcp_commands import *
-
-def privmsg_(handler, msg):
-    ''' Handles user messages (user/channel) '''
-    if msg.content[0] == handler.bot.cmd_prefix and len(msg.content) > 1:
-        try:
-            cmd = Command(handler.bot, msg)
-            command_dic[cmd.root](cmd)  # Runs command
-        except Exception as e:
->>>>>>> 3b02f0b451bd251f3a499a7b6950f482b62d8266
             handler.bot.log('Command exception: %s' % e)
